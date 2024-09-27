d5a1752e35602c396b02509824ac6af2a6ebc4dd
Ali Khan <alikhan@Alis-MacBook-Pro.local>
2024-09-27 14:48:29 -0500

    added a Registartion page and added a line

--- src/main/java/Pages/SearchPage.java
+++ src/main/java/Pages/SearchPage.java
@@ -4,5 +4,7 @@ public class SearchPage {
     public void searchMethod(){
         System.out.println("this is the search method");
         System.out.println("Searching Product");
+        System.out.println("Search Page");
+
     }
 }
