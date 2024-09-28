0383f98e730fd4a6f81b8dad0bdbf19d2f84054e
Ali Khan <alikhan@Alis-MacBook-Pro.local>
2024-09-28 17:29:05 -0500

    added cart page and registration page modified

---
+++ src/main/java/Pages/CartPage.java
@@ -0,0 +1,7 @@
+package Pages;
+
+public class CartPage {
+    public static void main(String[] args) {
+        System.out.println("THis is my Cart page");
+    }
+}
--- src/main/java/Pages/RegistrationPage.java
+++ src/main/java/Pages/RegistrationPage.java
@@ -5,5 +5,6 @@ public class RegistrationPage {
 
     public static void main(String[] args) {
         System.out.println(i);
+        System.out.println("This the variable");
     }
 }
