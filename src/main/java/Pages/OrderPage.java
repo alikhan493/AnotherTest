bd895b63e96a9c59622a406c76534aca733717ec
Ali Khan <alikhan@Alis-MacBook-Pro.local>
2024-09-30 07:20:46 -0500

    updated orderFeature upon ali comment's

--- src/main/java/Pages/OrderPage.java
+++ src/main/java/Pages/OrderPage.java
@@ -5,5 +5,7 @@ public class OrderPage {
 
     public void doOrder() {
         System.out.println("This is the order page");
+        String orderId = "123";
+        System.out.println(orderId);
     }
 }
